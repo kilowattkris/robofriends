17823e0ce651901ce69fc85c36d140d7c34c3367
kristine <ktomlinson@elltechnologies.com>
2021-09-16 18:24:26 -0400

    Clean out unused code

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,35 +6,14 @@ import Scroll from "../components/Scroll";
 import { useSelector, useDispatch } from 'react-redux';
 import {fetchRobots} from '../features/robots/robotsSlice';
 
-//import CardList from "../components/CardList";
 const CardList = React.lazy(() => import("../components/CardList"));
 
 const App = () => {
-  // const [robots, setRobots] = useState([]);
   const robots = useSelector((state) => state.robots.robots);
   const dispatch = useDispatch();
   const [search, setSearch] = useState("");
 
-  // dispatch(fetchRobots());
-
   useEffect(() => {
-    // const configUsers = users => {
-    //   return users.map(user => createImgSrc(user));
-    // };
-
-    // const createImgSrc = (user) => {
-    //   user.imgSrc = `https://robohash.org/${user.id}.png`;
-    //   user.imgAlt = user.name;
-    //   return user;
-    // };
-
-    // fetch('https://jsonplaceholder.typicode.com/users')
-    //   .then(response => response.json())
-    //   .then(users => {
-    //     //setRobots(configUsers(users));
-    //     dispatch(success(configUsers(users)));
-    //   })
-    //   .catch(error => dispatch(failed(error)));
     dispatch(fetchRobots())
   }, [dispatch]);
 
--- src/features/robots/robotsSlice.js
+++ src/features/robots/robotsSlice.js
@@ -3,11 +3,6 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchRobots = createAsyncThunk(
   "robots/fetchRobots",
-  // async(thunkAPI) => {
-  //   const response = await fetch(usersAPI);
-  //   console.log(response);
-  //   return response.json();
-  // }
   userAPI.fetchUsers
 );
 
@@ -21,21 +16,6 @@ export const robotsSlice = createSlice({
   name: 'robots',
   initialState,
   reducers: {
-    // pending: (state) => {
-    //   state.isPending = true;
-    //   // Redux Toolkit allows us to write "mutating" logic in reducers. It
-    //   // doesn't actually mutate the state because it uses the Immer library,
-    //   // which detects changes to a "draft state" and produces a brand new
-    //   // immutable state based off those changes
-    //   state.value += 1
-    // },
-    // success: (state, action) => {
-    //   state.robots = action.payload;
-    //   state.isPending = false;
-    // },
-    // failed: (state, action) => {
-    //   state.error = action.payload;
-    // },
   },
   extraReducers: (builder) => {
     // Add reducers for additional action types here, and handle loading state as needed
